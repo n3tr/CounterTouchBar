a0e2bab1fb5b6fa624961c94f7e1743e04b00413
Jirat K <saakyz@gmail.com>
2016-10-29 18:58:24 +0700

    Initial Commit

---
+++ CounterTouchBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CounterTouchBar
+//
+//  Created by Jirat Ki. on 10/29/2559 BE.
+//  Copyright © 2559 Jirat Ki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
